02820c1013aade27d1d17506d7b99930bb16074e
Emma Gerigscott <elgerig@gmail.com>
2023-11-08 10:50:50 -0800

    Refactor Languages class methods to use expression-bodied members.

--- TracksOnTracksOnTracks.cs
+++ TracksOnTracksOnTracks.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 
 public static class Languages
 {
-    public static List<string> NewList()
-    {
-        return new List<string>();
-    }
+    public static List<string> NewList() => new List<string>();
 
-    public static List<string> GetExistingLanguages()
-    {
-        return new List<string> { "C#", "Clojure", "Elm" };
-    }
+    public static List<string> GetExistingLanguages() => new List<string> { "C#", "Clojure", "Elm" };
 
     public static List<string> AddLanguage(List<string> languages, string language)
     {
@@ -19,15 +13,9 @@ public static class Languages
         return languages;
     }
 
-    public static int CountLanguages(List<string> languages)
-    {
-        return languages.Count;
-    }
+    public static int CountLanguages(List<string> languages) => languages.Count;
 
-    public static bool HasLanguage(List<string> languages, string language)
-    {
-        return languages.Contains(language);
-    }
+    public static bool HasLanguage(List<string> languages, string language) => languages.Contains(language);
 
     public static List<string> ReverseList(List<string> languages)
     {
@@ -37,27 +25,13 @@ public static class Languages
 
     public static bool IsExciting(List<string> languages)
     {
-        if (languages.Count == 0) {
+        if (languages.Count == 0)
+        {
             return false;
-        }
-        else if (languages[0] == "C#")
-        {
-            return true;
-        }
-        else if (languages.Count == 2 || languages.Count == 3)
-        {
-            if (languages[1] == "C#")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
         else
         {
-            return false;
+            return languages[0] == "C#" || (languages[1] == "C#" && (languages.Count == 2 || languages.Count == 3));
         }
     }
 
